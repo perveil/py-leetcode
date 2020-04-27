4541d495d16772a662b8f7e036eef893e5405e5e
perveil <986778566@qq.com>
2020-04-27 10:53:38 +0800

    firstday

---
+++ binarySearch/leetcode33_搜索旋转数组.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0;
+        right =len(nums)-1
+        while(left<=right):
+            mid=(left+right)>>1;
+            if nums[mid]==target:
+                return mid
+            if nums[mid]>=nums[left]: #左侧有序
+                if nums[mid]>target and nums[left]<=target:
+                    right=mid-1
+                else:
+                    left=mid+1
+            else: #右侧升序
+                if nums[mid]<target and nums[right]>=target:
+                    left=mid+1
+                else:
+                    right=mid-1
+        return -1
